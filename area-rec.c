9fa621b88cf76c7d511bc096b8600242b1b09e2b
Aditi Jain <aj3238854@gmail.com>
2026-07-26 22:14:32 +0530

    Add files via upload

---
+++ area-rec.c
@@ -0,0 +1,10 @@
+//to find the area of rectangle
+#include<stdio.h>
+void main()
+{
+	float l,b,area;
+	printf("enter the value of Length and Bredth");
+	scanf("%f%f",&l,&b);
+	area = l*b;
+	printf("area of rectangle= %f",area); 
+}
